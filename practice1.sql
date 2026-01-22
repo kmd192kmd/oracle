eb3e125d8cb102c98dde4338eebecd0fb4046f4c
pinn.public <pinn.public@gmail.com>
2026-01-22 17:47:25 +0900

    오라클 문제01 - 5번까지 풂

--- practice1.sql
+++ practice1.sql
@@ -11,4 +11,19 @@ select
     area as "[면적]",
     continent as "[대륙]"
 from tblCountry;
+
+select
+    ('국가명: ' || name || ', 수도명: ' || capital || ', 인구수: ' || population) as "[국가정보]"
+from tblCountry;
+
+select * from employees;
+ 
+select -- 문제01.sql의 5번까지 함.
+    first_name || last_name as "[이름]",
+    email as "[이메일]",
+    phone_number as "[연락처]",
+    salary as "[급여]"
+from employees;
+ 
+ 
     
